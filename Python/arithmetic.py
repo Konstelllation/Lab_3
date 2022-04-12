5bd00e37da455690434fd3ab970d52fe7c8aabc2
Konstelllation <harchenkobo2003@gmail.com>
2022-04-12 13:42:25 +0300

    Codes for tasks

---
+++ Python/arithmetic.py
@@ -0,0 +1,5 @@
+print ("4 * 100 - 54 =")
+print("Please,write an answer:")
+answer = int(input())
+print ("Correct answer is",4 * 100 - 54)
+print ("Your answer:",answer)
---
+++ Python/user.py
@@ -0,0 +1,10 @@
+print("What is your name?")
+name = str(input())
+print ("How old are you?")
+age = int(input())
+print ("Where are you from?")
+place = str(input())
+print ("This is ",name)
+print ("(S)he is" ,age,"years old")
+print ("(S)he live in",place)
+
